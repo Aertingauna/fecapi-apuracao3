b4484b96796b8d4a9213512431180b3b7cf10d92
Aertingauna <tingauna2@gmail.com>
2025-04-09 12:47:47 -0300

    Create app.py
    ok

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="FECAPI - Sistema de Apuração", layout="wide")
+
+st.title("📊 Sistema de Apuração - FECAPI")
+st.write("Bem-vindo ao sistema de apuração de competições de Capoeira da FECAPI.")
